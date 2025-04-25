56f9e1f8ced7998c34d3b4c5d69269c73f929899
carlinhosantosjr <carlinhosantosjr@hotmail.com>
2025-04-25 10:29:12 -0300

    Add core components ...

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -9,12 +9,14 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
     '@chromatic-com/storybook',
+    "@storybook/addon-interactions",
+     "@storybook/addon-a11y",
     "@storybook/experimental-addon-test"
   ],
   "framework": {
     "name": '@storybook/react-vite',
     "options": {}
-  }
+  },
 };
 export default config
 
